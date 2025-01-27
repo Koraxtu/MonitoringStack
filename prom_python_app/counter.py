eb0c57c85609041af9a0163bb740bf082bfb2e15
Koraxtu <bukason2013@gmail.com>
2025-01-27 07:00:09 -0500

    Testing branch

--- prom_python_app/counter.py
+++ prom_python_app/counter.py
@@ -1,11 +1,14 @@
 import http.server
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Counter
+
+REQUEST_COUNT = Counter('app_request_count', 'total all http request count')
 
 APP_PORT = 8000
 METRICS_PORT = 8001
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUEST_COUNT.inc()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -15,5 +18,5 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     start_http_server(METRICS_PORT)
-    server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    server = http.server.HTTPServer(('0.0.0.0', APP_PORT), HandleRequests)
     server.serve_forever()
